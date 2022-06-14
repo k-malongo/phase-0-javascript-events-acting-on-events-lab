d9634110b8d02bc07018b385f82631478fefa52d
Kelvin <vokemalosh32@gmail.com>
2022-06-14 11:01:05 +0300

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
 // Your code here
 const dodger = document.getElementById("dodger");
-// dodger.style.backgroundColor = "#000000";
-// dodger.style.backgroundColor = "#FF69B4";
-// dodger.style.bottom = "100px";
-// dodger.style.bottom = "0px";
-// dodger.style.left = "0px";
-// document.addEventListener("keydown", function (event) {
-//     console.log(event);
 
-//     if (event.key === "ArrowLeft") {
-//       const leftNumbers = dodger.style.left.replace("px", "");
-//       const left = parseInt(leftNumbers, 10);
-  
-//       dodger.style.left = `${left - 1}px`;
-//     }
-//   });
 function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
@@ -41,13 +27,14 @@ function moveDodgerRight() {
         if (e.key === "ArrowRight") {
             moveDodgerRight();
         }
-    }); 
-    function moveDodgerTop() {
-        const topNumbers = dodger.style.top.replace("px", "");
-        const bottom = parseInt(topNumbers, 10);
+    });
+
+ function moveDodgerTop() {
+    const topNumbers = dodger.style.top.replace("px", "");
+    const bottom = parseInt(topNumbers, 10);
     
-        if (bottom > 15) {
-          dodger.style.bottom = `${bottom + 1}px`;
+      if (bottom > 0) {
+        dodger.style.bottom = `${bottom + 1}px`;
         }
       }
       document.addEventListener("keydown", function(e) {
